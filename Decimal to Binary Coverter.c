46ccf3d3a69bd16e9c872230993eff602f9226ac
Md.Abdullah Al Rakib <122741559+iamrakib087@users.noreply.github.com>
2023-05-09 15:11:09 +0600

    Add files via upload

---
+++ Decimal to Binary Coverter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int a,n,aux[32],i=0;
+printf("Enter Any Number:");
+scanf("%d",&n);
+while(n>0){
+    aux[i++]=n%2;
+    n=n/2;
+}
+for(a=i-1; a>=0; a--)
+printf("%d",aux[a]);
+return 0;
+}
