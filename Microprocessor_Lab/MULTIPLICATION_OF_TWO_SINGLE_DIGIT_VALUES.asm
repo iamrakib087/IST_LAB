17c5a8d67b93e951be60e08bbe91ea6004cd58db
Md.Abdullah Al Rakib <122741559+iamrakib087@users.noreply.github.com>
2025-01-21 12:59:17 +0600

    Add files via upload

---
+++ Microprocessor_Lab/MULTIPLICATION_OF_TWO_SINGLE_DIGIT_VALUES.asm
@@ -0,0 +1,44 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    MSG1 DB 'ENTER TWO INTEGER NUMBERS: ','$'
+    MSG2 DB 'MULTIPLIED RESULT IS: ','$'
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    MOV AH,09
+    LEA DX,MSG1
+    INT 21H
+    
+    MOV AH,01
+    INT 21H
+    MOV BL,AL
+    INT 21H
+    
+    ;MULTIPLICATION
+    SUB BL,30H
+    SUB AL,30H
+    MUL BL ;CALCULATION  AX = AL*BL
+    MOV BX,AX
+    
+    ;NEWLINE
+    MOV AH,02
+    MOV DL,10
+    INT 21H
+    MOV DL,13
+    INT 21H
+    
+    ;OUTPUT
+    MOV AH,09
+    LEA DX,MSG2
+    INT 21H
+    MOV AH,02
+    ADD BX,30H
+    MOV DX,BX
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
